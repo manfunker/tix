125462ae17373419de96a668c5563d1eb234cfce
manfunker <funkdust@gmail.com>
2014-09-15 22:19:28 -0300

    Update Turismo.java

--- src/turismo/Turismo.java
+++ src/turismo/Turismo.java
@@ -3,14 +3,7 @@ package turismo;
 public class Turismo {
 
     public static void main(String[] args) {
-        //responder si vieron esta modificacion
-        
-        //nos fuimos a la b
-        
-        //csm tamos negros!
-        
-        //el mati se la come donde anda el weon:C
-        //nose wn q l4 chupe
+       
     }
     
 }
